5236e7b332616257c765ad0e729195fc35510164
yasar2385 <yasar.mohideen@newgen.co>
2021-08-25 09:48:33 +0530

    2 files modified

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 // Import stylesheets
 import './style.css';
 
+function after() {
+  var list = document.getElementById('li-3');
+  list.insertAfter();
+}
+
+Element.prototype.insertAfter = function(node) {
+  console.log('start');
+  if (node.nextSibling) {
+    node.parentNode.insertBefore(this, node.nextSibling);
+  } else {
+    node.parentNode.appendChild(this);
+  }
+  console.log('end');
+};
+
 // Write Javascript code!
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
@@ -11,57 +26,46 @@ const item = document.querySelector('.item');
 item.addEventListener('dragstart', dragStart);
 
 function dragStart(e) {
-    e.dataTransfer.setData('text/plain', e.target.id);
-    setTimeout(() => {
-        e.target.classList.add('hide');
-    }, 0);
+  e.dataTransfer.setData('text/plain', e.target.id);
+  setTimeout(() => {
+    e.target.classList.add('hide');
+  }, 0);
 }
-
 
 /* drop targets */
 const boxes = document.querySelectorAll('.box');
 
 boxes.forEach(box => {
-    box.addEventListener('dragenter', dragEnter)
-    box.addEventListener('dragover', dragOver);
-    box.addEventListener('dragleave', dragLeave);
-    box.addEventListener('drop', drop);
+  box.addEventListener('dragenter', dragEnter);
+  box.addEventListener('dragover', dragOver);
+  box.addEventListener('dragleave', dragLeave);
+  box.addEventListener('drop', drop);
 });
 
-
 function dragEnter(e) {
-    e.preventDefault();
-    e.target.classList.add('drag-over');
+  e.preventDefault();
+  e.target.classList.add('drag-over');
 }
 
 function dragOver(e) {
-    e.preventDefault();
-    e.target.classList.add('drag-over');
+  e.preventDefault();
+  e.target.classList.add('drag-over');
 }
 
 function dragLeave(e) {
-    e.target.classList.remove('drag-over');
+  e.target.classList.remove('drag-over');
 }
 
 function drop(e) {
-    e.target.classList.remove('drag-over');
+  e.target.classList.remove('drag-over');
 
-    // get the draggable element
-    const id = e.dataTransfer.getData('text/plain');
-    const draggable = document.getElementById(id);
+  // get the draggable element
+  const id = e.dataTransfer.getData('text/plain');
+  const draggable = document.getElementById(id);
 
-    // add it to the drop target
-    e.target.appendChild(draggable);
+  // add it to the drop target
+  e.target.appendChild(draggable);
 
-    // display the draggable element
-    draggable.classList.remove('hide');
+  // display the draggable element
+  draggable.classList.remove('hide');
 }
-
-
-Element.prototype.insertAfter = function() {
-    if (this.parentNode.nextSibling) {
-        this.parentNode.parentNode.insertBefore(this, this.parentNode.nextSibling);
-    } else {
-        this.parentNode.parentNode.appendChild(this);
-    }
-};
